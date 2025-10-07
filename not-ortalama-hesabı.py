00750f8915398c8098578f7e22f6e57a2c258347
Nazlıcan Yavuz <nazlicanyavuz000@gmail.com>
2025-10-07 11:41:56 +0300

    Not ortalama hesabı kodu eklendi

---
+++ not-ortalama-hesabı.py
@@ -0,0 +1,9 @@
+vize = float(input("Vize Notu:"))
+proje = float(input("Proje Notu:"))
+final = float(input("Final Notu:"))
+ortalama = (vize * 0.30) + (proje * 0.30) + (final * 0.40)
+if ortalama >=50:
+    sonuc = "GEÇTİ"
+else:
+    sonuc = "KALDI"
+print(sonuc)
